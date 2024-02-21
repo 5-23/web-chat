b40703013a9da546be12961268a3df32dfa82463
5-23 <yhanbyeol6@gmail.com>
2024-02-21 14:47:08 +0900

    feat: add publish.yml, style

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> std::io::Result<()> {
     use actix_web::*;
     use leptos::*;
     use leptos_actix::{generate_route_list, LeptosRoutes};
-    use lama::app::*;
+    use web_chat::app::*;
 
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
--- src/pages/index.rs
+++ src/pages/index.rs
@@ -3,10 +3,12 @@ use leptos::*;
 pub fn Index() -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
         <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <button
+            style:color="#000"
+            on:click= move |_| set_count.update(|count| *count += 1)
+        >"Click Me: " {count}</button>
     }
 }
