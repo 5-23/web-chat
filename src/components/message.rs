353eecd383dbde14f592ea52f063bbc5c1c5b96e
5-23 <yhanbyeol6@gmail.com>
2024-02-22 21:27:36 +0900

    feat: add send message system

--- src/components/message.rs
+++ src/components/message.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 
 #[component]
-pub fn Message(id: &'static str, content: &'static str) -> impl IntoView { 
+pub fn Message(id: String, content: String) -> impl IntoView { 
     view! {
         <div class="message">
             <p id="user">{id}</p>:
--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,15 +1,28 @@
-use leptos::*;
-use leptos_router::ActionForm;
+use leptos::{html::Input, *};
 use crate::components::*;
 #[component]
 pub fn Index() -> impl IntoView {
+    let content_ref = NodeRef::<Input>::new();
     // Creates a reactive value to update the button
-    let (messages, set_messages) = create_signal(vec![("sus", "this is message")]);
+    // Vec::<(String, String)>::new()
+    let (messages, set_messages) = create_signal(vec![
+        ("a".to_string(), "a".to_string()),
+        ("a".to_string(), "a".to_string()),
+        ("b".to_string(), "b".to_string()),
+        ("c".to_string(), "c".to_string()),
+        ("d".to_string(), "d".to_string()),
+    ]);
 
     view! {
         <main>
-            <form>
-                <input type="text" placeholder="sus"/>
+            <form on:submit=move |e| {
+                e.prevent_default();
+                let mut new_value = messages.get().clone();
+                new_value.push(("sus".to_string(), content_ref.get().unwrap().value()));
+                set_messages.set(new_value);
+                logging::log!("{:?} {:?}", messages.get().as_ptr(), messages.get());
+            }>
+                <input type="text" _ref={content_ref} placeholder="sus"/>
                 <input type="submit"/>
             </form>
 
@@ -17,9 +30,9 @@ pub fn Index() -> impl IntoView {
             each=move || messages.get()
             key=|n| n.clone()
             let:data
-        >
-            <Message id={data.0} content={data.1}/>
-        </For>
+            children=|data|view! { <Message id={data.0} content={data.1}/> }
+        />
+            
         
         </main>
         
