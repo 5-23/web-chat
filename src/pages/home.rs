9eec2ef8ae365b82c77daab914c63f3d287f1178
5-23 <yhanbyeol6@gmail.com>
2024-02-27 21:20:37 +0900

    feat: change some

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -7,12 +7,7 @@ pub fn Home() -> impl IntoView {
     view! {
         <div class="container">
 
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
-
-            <h1>"Welcome to Leptos"</h1>
+            <h1>"Hello, World :D"</h1>
 
             <div class="buttons">
                 <Button />
