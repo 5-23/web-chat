22913b60ae068eb66a475784c5fe96c4a691c236
5-23 <yhanbyeol6@gmail.com>
2024-03-18 16:46:26 +0900

    feat: final

---
+++ main.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+
+// WebSocket 서버 생성
+const wss = new WebSocket.Server({ port: 3000 });
+
+// 연결된 모든 클라이언트 저장할 배열
+const clients = [];
+
+// 클라이언트 연결 시 이벤트 리스너
+wss.on('connection', function connection(ws) {
+  // 연결된 클라이언트를 배열에 추가
+  clients.push(ws);
+
+  // 클라이언트로부터 메시지 수신 시 이벤트 리스너
+  ws.on('message', function incoming(message) {
+    ws.send(message.toString());
+    // 연결된 모든 클라이언트에게 메시지 전송
+    clients.forEach(function(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message.toString()); // Send message to each client except the sender
+        }
+    });
+  });
+
+  // 클라이언트 연결 종료 시 이벤트 리스너
+  ws.on('close', function close() {
+    // 배열에서 해당 클라이언트 제거
+    clients.splice(clients.indexOf(ws), 1);
+  });
+});
